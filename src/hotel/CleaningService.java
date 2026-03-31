3147e9c7b254a695ec010b49191c5ded2229810a
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:33:49 +0500

    Update CleaningService.java

--- src/hotel/CleaningService.java
+++ src/hotel/CleaningService.java
@@ -1,4 +1,11 @@
 package hotel;
 
 public class CleaningService {
+    public void scheduleCleaning(String roomType) {
+        System.out.println("Уборка для " + roomType + " запланирована.");
+    }
+
+    public void performCleaning(String roomType) {
+        System.out.println("Номер " + roomType + " убран.");
+    }
 }

3dc35247f44abbcc1261bc37801766102fbe7576
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:34:25 +0500

    Update Client.java

--- src/hotel/Client.java
+++ src/hotel/Client.java
@@ -1,4 +1,15 @@
 package hotel;
 
 public class Client {
+    public static void main(String[] args) {
+        HotelFacade hotel = new HotelFacade();
+
+        hotel.bookRoomWithServices("Deluxe", "Суши");
+        hotel.organizeEvent("Конференция", "Проектор", "Suite");
+        hotel.bookTableWithTaxi(4);
+
+        hotel.cancelRoomBooking("Deluxe");
+        hotel.cancelEvent("Конференция");
+        hotel.cancelFoodOrder();
+    }
 }

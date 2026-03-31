4bea9827211b3e6989f096c40cc5eed0708a29d0
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:33:16 +0500

    Update RestaurantSystem.java

--- src/hotel/RestaurantSystem.java
+++ src/hotel/RestaurantSystem.java
@@ -1,4 +1,15 @@
 package hotel;
 
 public class RestaurantSystem {
+    public void bookTable(int people) {
+        System.out.println("Стол на " + people + " человек забронирован.");
+    }
+
+    public void orderFood(String food) {
+        System.out.println("Заказано: " + food);
+    }
+
+    public void cancelOrder() {
+        System.out.println("Заказ отменен.");
+    }
 }

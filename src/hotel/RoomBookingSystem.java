d1721761de09f617431dd78d489a435f53174dbf
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:33:02 +0500

    Update RoomBookingSystem.java

--- src/hotel/RoomBookingSystem.java
+++ src/hotel/RoomBookingSystem.java
@@ -1,4 +1,15 @@
 package hotel;
 
 public class RoomBookingSystem {
+    public void bookRoom(String roomType) {
+        System.out.println("Номер " + roomType + " забронирован.");
+    }
+
+    public void cancelBooking(String roomType) {
+        System.out.println("Бронирование номера " + roomType + " отменено.");
+    }
+
+    public boolean isAvailable(String roomType) {
+        return true; 
+    }
 }

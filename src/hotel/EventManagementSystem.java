08ed3b617a0eee771be5a9100d0793d847cb01ad
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:33:32 +0500

    Update EventManagementSystem.java

--- src/hotel/EventManagementSystem.java
+++ src/hotel/EventManagementSystem.java
@@ -1,4 +1,15 @@
 package hotel;
 
 public class EventManagementSystem {
+    public void bookEvent(String eventName) {
+        System.out.println("Мероприятие '" + eventName + "' забронировано.");
+    }
+
+    public void bookEquipment(String equipment) {
+        System.out.println("Оборудование '" + equipment + "' забронировано.");
+    }
+
+    public void cancelEvent(String eventName) {
+        System.out.println("Мероприятие '" + eventName + "' отменено.");
+    }
 }

66c2ad84b5d6cf15ba9e75214d5247476ccd3050
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:34:42 +0500

    Update OrganizationComponent.java

--- src/organization/OrganizationComponent.java
+++ src/organization/OrganizationComponent.java
@@ -1,4 +1,13 @@
 package organization;
 
-public class OrganizationComponent {
+public abstract class OrganizationComponent {
+    protected String name;
+
+    public OrganizationComponent(String name) {
+        this.name = name;
+    }
+
+    public abstract void showDetails();
+    public abstract double getBudget();
+    public abstract int getEmployeeCount();
 }

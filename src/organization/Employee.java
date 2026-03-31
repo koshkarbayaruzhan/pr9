c040115226a66c805b400aa1276305e82d9caba5
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:34:56 +0500

    Update Employee.java

--- src/organization/Employee.java
+++ src/organization/Employee.java
@@ -1,4 +1,33 @@
 package organization;
 
-public class Employee {
+public class Employee extends OrganizationComponent {
+    private String position;
+    private double salary;
+    private boolean isContractor;
+
+    public Employee(String name, String position, double salary, boolean isContractor) {
+        super(name);
+        this.position = position;
+        this.salary = salary;
+        this.isContractor = isContractor;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println(name + " - " + position + " - Зарплата: " + salary + (isContractor ? " (Контракт)" : ""));
+    }
+
+    @Override
+    public double getBudget() {
+        return isContractor ? 0 : salary;
+    }
+
+    @Override
+    public int getEmployeeCount() {
+        return 1;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }

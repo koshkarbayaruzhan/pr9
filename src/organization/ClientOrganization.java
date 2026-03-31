d4d213099a7a3742b7bb6bebcfc970349f02d94f
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:35:32 +0500

    Update ClientOrganization.java

--- src/organization/ClientOrganization.java
+++ src/organization/ClientOrganization.java
@@ -1,4 +1,21 @@
 package organization;
 
 public class ClientOrganization {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Иван", "Разработчик", 1000, false);
+        Employee e2 = new Employee("Мария", "Аналитик", 1200, false);
+        Employee e3 = new Employee("Пётр", "Фрилансер", 500, true);
+
+        Department devDept = new Department("Разработка");
+        devDept.addComponent(e1);
+        devDept.addComponent(e3);
+
+        Department itDept = new Department("IT");
+        itDept.addComponent(devDept);
+        itDept.addComponent(e2);
+
+        itDept.showDetails();
+        System.out.println("Общий бюджет отдела: " + itDept.getBudget());
+        System.out.println("Общее количество сотрудников: " + itDept.getEmployeeCount());
+    }
 }

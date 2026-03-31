bd4f997d1a47b5f709493d58fa542c0999d69ac3
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-31 17:35:15 +0500

    Update Department.java

--- src/organization/Department.java
+++ src/organization/Department.java
@@ -1,4 +1,46 @@
 package organization;
 
-public class Department {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department extends OrganizationComponent {
+    private List<OrganizationComponent> components = new ArrayList<>();
+
+    public Department(String name) {
+        super(name);
+    }
+
+    public void addComponent(OrganizationComponent component) {
+        components.add(component);
+    }
+
+    public void removeComponent(OrganizationComponent component) {
+        components.remove(component);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Отдел: " + name);
+        for(OrganizationComponent c : components) {
+            c.showDetails();
+        }
+    }
+
+    @Override
+    public double getBudget() {
+        double budget = 0;
+        for(OrganizationComponent c : components) {
+            budget += c.getBudget();
+        }
+        return budget;
+    }
+
+    @Override
+    public int getEmployeeCount() {
+        int count = 0;
+        for(OrganizationComponent c : components) {
+            count += c.getEmployeeCount();
+        }
+        return count;
+    }
 }
